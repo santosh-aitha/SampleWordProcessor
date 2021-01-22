2aba049bec821e684b2f43a70d80c00780f028bb
santosh-aitha <77001350+santosh-aitha@users.noreply.github.com>
2021-01-22 21:38:59 +0530

    Add files via upload

--- Controllers/WordController.cs
+++ Controllers/WordController.cs
@@ -13,8 +13,8 @@ using Microsoft.Extensions.Options;
 
 namespace SampleParser.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
+   
+    [ApiController]   
     public class WordController : ControllerBase
     {
         private readonly ILogger<WordController> _logger;
@@ -24,81 +24,59 @@ namespace SampleParser.Controllers
             _logger = logger;
         }
 
-        const string path = @"..\SampleParser\localData\haiwatha.txt";
-        [HttpGet]
+        const string path = @"localData\haiwatha.txt";
+        [HttpGet("Word")]
+        ///The api reference is used for parsing the entire text file and then find out the number of occurences of each of the words
         public IEnumerable<WordCount> Get()
         {
-            var result = GetWordOccurences(ReadFile(path));
+            var result = FileHelper.GetWordOccurences(FileHelper.ReadFile(path));
             return result.Select(wc => new WordCount { Word = wc.Word, Count = wc.Count }).OrderBy(x => x.Count).ToArray();
 
         }
 
-        [Route("Top10Words")]
-        [HttpGet]
+        [HttpGet("Top10Words")]
+        ///The api reference is used for parsing the entire text file and then find out the Top 10 occurences of words
         public IEnumerable<WordCount> GetTop10Occurences()
         {
-            var result = GetWordOccurences(ReadFile(path));
+            var result = FileHelper.GetWordOccurences(FileHelper.ReadFile(path));
             return result.Select(wc => new WordCount { Word = wc.Word, Count = wc.Count }).OrderByDescending(x => x.Count).ToArray().Take(10);
 
         }
 
-        [Route("GetWordCount")]
-        [HttpGet]
+       
+        [HttpGet("GetWordCount")]
+        ///The api reference is used for parsing the entire text file and then find out the word that is supplied as querystring param and displays occurences of word
         public IEnumerable<WordCount> GetWordWithCount(string word)
         {
-            var result = GetWordOccurences(ReadFile(path));
-            return result.Where(x => x.Word.Equals(word)).Select(y => y); 
-              
-             
+            var result = FileHelper.GetWordOccurences(FileHelper.ReadFile(path));
+            return result.Where(x => x.Word.Equals(word)).Select(y => y);
+
+
         }
 
-        [Route("GetWordDefinition")]
-        [HttpGet]
+       
+        [HttpGet("GetWordDefinition")]
+        ///The api reference is used contact OwlBot apis by passing a word as querystring parameter and gets back the definition of the word if found
         public async Task<IEnumerable<WordCount>> GetWordWithCountAsync(string word)
         {
-            var result = GetWordOccurences(ReadFile(path));
+            var result = FileHelper.GetWordOccurences(FileHelper.ReadFile(path));
             WordCount resultWord = new WordCount();
             if (result.Count() > 0)
             {
                 resultWord = result.Where(x => x.Word.Equals(word)).Select(y => y).FirstOrDefault();
                 if (!string.IsNullOrEmpty(resultWord.Word))
                 {
-                    await GetOwlDefinitionsAsync(resultWord.Word);
+                    await OwlAPIHelper.GetOwlDefinitionsAsync(resultWord.Word);
                 }
 
             }
             return result;
         }
 
-        public async Task<string> GetOwlDefinitionsAsync(string word)
-        {
-            string endPoint = $"{Constants.OWL_ENDPOINT}{word}";
-            var client = new HttpClient();
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Constants.ACCESS_TOKEN);
-            HttpResponseMessage response = await client.GetAsync(endPoint);
-            response.EnsureSuccessStatusCode();
-            return await response.Content.ReadAsStringAsync();
 
-        }
-        public string ReadFile(string path)
-        {
-            StreamReader sr = new StreamReader(path);
-            return System.IO.File.ReadAllText(path);
-        }
 
-        public IEnumerable<WordCount> GetWordOccurences(string inputString)
-        {
-            var result = inputString.Split(new char[] { '.', '?', '!', ' ', ';', ':', ',', '\"' }, StringSplitOptions.RemoveEmptyEntries)
-                 .GroupBy(r => r.Trim(), StringComparer.InvariantCultureIgnoreCase)
-                 .Where(grp => grp.Count() >= 0)
-                 .Select(grp => new WordCount
-                 {
-                     Word = grp.Key,
-                     Count = grp.Count()
 
-                 }).ToArray();
-            return result;
-        }
+
 
     }
 }
