e8552725e9fa95a6ed491138d5396675b6a07b03
santosh-aitha <77001350+santosh-aitha@users.noreply.github.com>
2021-01-22 21:39:59 +0530

    Update Constants.cs

--- Constants.cs
+++ Constants.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,6 +12,8 @@ namespace SampleParser
         public const string FILE_NAME = "haiwatha.txt";
         public const string OWL_ENDPOINT = @"https://owlbot.info/api/v4/dictionary/";
         public const string ACCESS_TOKEN = "609b538891053085d73a526a94fb3b0ae57ee63d";
+        public const string SwaggerRoute = "/swagger/ui/index";
 
     }
 }
+
