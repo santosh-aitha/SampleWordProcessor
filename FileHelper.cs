24fa6b2172ea2045ad75fa62cbd519a9880317f6
santosh-aitha <77001350+santosh-aitha@users.noreply.github.com>
2021-01-22 21:40:27 +0530

    Update FileHelper.cs

--- FileHelper.cs
+++ FileHelper.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,19 +8,51 @@ namespace SampleParser
 {
     public static class FileHelper
     {
+        public static string ReadFile(string path)
+        {
+            try
+            {
+                if (VerifyFileExists(path))
+                {
+                    StreamReader sr = new StreamReader(path);
+                    return System.IO.File.ReadAllText(path);
+                }
+                else {
+                    throw new FileNotFoundException();
+                }
+            }
+            catch
+            {
+                throw new FileNotFoundException();
+            }
+        }
 
+
+        public static IEnumerable<WordCount> GetWordOccurences(string inputString)
+        {
+            try
+            {
+                var result = inputString.Split(new char[] { '.', '?', '!', ' ', ';', ':', ',', '\"' }, StringSplitOptions.RemoveEmptyEntries)
+                 .GroupBy(r => r.Trim(), StringComparer.InvariantCultureIgnoreCase)
+                 .Where(grp => grp.Count() >= 0)
+                 .Select(grp => new WordCount
+                 {
+                     Word = grp.Key,
+                     Count = grp.Count()
+
+                 }).ToArray();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
         public static bool VerifyFileExists(string path)
         {
-
-
-            return false;        
+            return File.Exists(path);
         }
 
-        public static bool VerifyFolderExists(string path)
-        {
-
-
-            return false;
-        }
+      
     }
 }
